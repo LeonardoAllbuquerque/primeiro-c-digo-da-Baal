ced4a623f06a8b09b6a48c4b0f53866311cc060b
Your Name <you@example.com>
2023-07-11 18:04:51 -0300

    firstcommit

---
+++ styles/alura.js
@@ -0,0 +1,8 @@
+alert('Raiden Shogun é MINHA mommy!')
+var visões_adiquiridas = parseInt(prompt("Quantas visões vc tem?"))
+if(visões_adiquiridas>0)
+alert("Você possui "+ visões_adiquiridas+ " a mais que o permitido!" )
+if(visões_adiquiridas==0)
+alert("PARABÉNS, você é um cidadão exemplar!")
+if(visões_adiquiridas<0)
+alert("Fic")
